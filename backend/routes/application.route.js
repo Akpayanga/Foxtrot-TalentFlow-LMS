5c94ff906efcdaa95334325be701164a64d68d8c
paraclete12 <oliverparaclete@gmail.com>
2026-04-03 09:46:59 +0100

    Revert "Resolve merge conflict: feature/dashboard-backend into dev"
    This reverts commit fab7ccdac7741283f694fe64e6941f45d5f74e06, reversing
    changes made to 6924df8b6a82a69ab89869e368ac346401563502.

--- backend/routes/application.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const applicationController = require("../controllers/application.controller");
-
-router.post("/submit", applicationController.submitApplication);
-router.get("/status/:email", applicationController.getApplicationStatus);
-
-module.exports = router;
