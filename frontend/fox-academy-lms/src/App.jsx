44cbc0511580e0ad45069401a6744c78890d4a17
iamnftlord <afolabigbenga1995@gmail.com>
2026-04-08 11:16:03 +0100

    conflict-resolve

--- frontend/fox-academy-lms/src/App.jsx
+++ frontend/fox-academy-lms/src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Start from "./pages/Start";
+import SignupCreate from "./pages/SignupCreate";
 import ForgotPassword from "./pages/ForgotPassword";
 import ForgotPasswordSent from "./pages/ForgotPasswordSent";
 import EmailVerification from "./pages/EmailVerification";
@@ -13,23 +13,9 @@ import LearnPreview from "./pages/LearnPreview";
 import OnboardingComplete from "./pages/OnboardingComplete";
 import ApplicationForm from "./pages/ApplicationForm";
 import ApplicationReceived from "./pages/ApplicationReceived";
-<<<<<<< HEAD
 import WatchCourse from "./pages/WatchCourse";
 
 
-=======
-import ApplicationAccepted from "./pages/ApplicationAccepted";
-import Dashboard from "./pages/Dashboard";
-import MyLearning from "./pages/MyLearning";
-import ModuleDetail from "./pages/ModuleDetail";
-import Assignments from "./pages/Assignments";
-import UserPersonaAssignment from "./pages/UserPersonaAssignment";
-import Resources from "./pages/Resources";
-import Community from "./pages/Community";
-import DiscussionDetail from "./pages/DiscussionDetail";
-import CreateDiscussion from "./pages/CreateDiscussion";
-import Progress from "./pages/Progress";
->>>>>>> feature/start-and-application-success
 
 function App() {
   return (
@@ -38,24 +24,11 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/start" element={<Start />} />
+        <Route path="/signup/create" element={<SignupCreate />} />
         <Route path="/verify-email" element={<EmailVerification />} />
         <Route path="/application" element={<ApplicationForm />} />
         <Route path="/application/success" element={<ApplicationReceived />} />
         <Route path="/application/success/link/:token" element={<ApplicationReceived />} />
-        <Route path="/application/accepted" element={<ApplicationAccepted />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/mylearning" element={<MyLearning />} />
-        <Route path="/mylearning/module" element={<ModuleDetail />} />
-        <Route path="/assignments" element={<Assignments />} />
-        <Route path="/assignments/user-persona" element={<UserPersonaAssignment />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/community" element={<Community />} />
-        <Route path="/community/discussion/:id" element={<DiscussionDetail />} />
-        <Route path="/community/create" element={<CreateDiscussion />} />
-        <Route path="/progress" element={<Progress />} />
-        
-        {/* Onboarding & Password Flow */}
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/forgot-password/sent" element={<ForgotPasswordSent />} />
         <Route path="/welcome" element={<Welcome />} />
@@ -63,6 +36,7 @@ function App() {
         <Route path="/onboarding/team" element={<JoinTeam />} />
         <Route path="/onboarding/learn" element={<LearnPreview />} />
         <Route path="/onboarding/complete" element={<OnboardingComplete />} />
+        <Route path="/watch-course" element={<WatchCourse />} />
 
 
 
